357cfc63622e5af6f0bade194cf8bf6b223945b0
jahid777 <66377960+jahid777@users.noreply.github.com>
2020-09-15 21:18:08 +0600

    user information api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
     
     <Counter></Counter>
-    
+    <User></User>
     </div>
   );
 }
+//this is for users api
+function User(){
+const [users, setUser] = useState([]);
+useEffect(() =>{
+  fetch ("https://jsonplaceholder.typicode.com/todos")
+  .then(res => res.json())
+  .then(data => setUser(data));
+},[])
 
 
+  return(
+    <div>
+      <h3>Dynamic users:{users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.title}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+//this is for btn for increase and decrease
 function Counter(){
  const [count, setCount] = useState(0);
  const handleIncrease = () => {
